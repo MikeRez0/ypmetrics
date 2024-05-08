ce6ca0373b012bc90704242ec5e9ccc1f448cb7d
MikeRe85 <45215419+MikeRe85@users.noreply.github.com>
2024-05-08 09:52:45 +0300

    gin added

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"net/http"
+	// "net/http"
+	"github.com/gin-gonic/gin"
 
 	"github.com/MikeRez0/ypmetrics/internal/handlers"
 	"github.com/MikeRez0/ypmetrics/internal/storage"
@@ -18,8 +19,13 @@ func run() error {
 	var ms = storage.NewMemStorage()
 	var h = handlers.NewMetricsHandler(ms)
 
-	mux := http.NewServeMux()
-	mux.Handle("/", h)
+	r := gin.Default()
+	r.POST("/update/:metricType/:metric/:value", h.UpdateMetricGin)
+	r.GET("/value/:metricType/:metric", h.GetMetricGin)
 
-	return http.ListenAndServe(`:8080`, mux)
+	return r.Run()
+	// mux := http.NewServeMux()
+	// mux.Handle("/", h)
+
+	// return http.ListenAndServe(`:8080`, mux)
 }
